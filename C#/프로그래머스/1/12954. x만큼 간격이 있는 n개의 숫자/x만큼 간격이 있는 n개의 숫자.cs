5a40440eee0e005edc111e12f80603d1d635a018
오수호 <suho.oh1213@gmail.com>
2025-11-07 07:52:50 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.52 ms, Memory: 31 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+
+public class Solution {
+    public List<long> solution(int x, int n) {
+        List<long> answer = new List<long>();
+        for(int i=1; i <= n ; i++)
+        {
+            
+            answer.Add((long)x * (long)i);
+        }
+        return answer;
+    }
+}
